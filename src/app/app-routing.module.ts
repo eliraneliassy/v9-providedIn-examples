f7e04837109c40ae7f7da047ce3c6790f351336f
Eliran Eliassy <eliran.eliassy@gmail.com>
2020-03-21 12:22:28 +0200

    project scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'a', loadChildren: () => import('./a/a.module').then(m => m.AModule) },
+  { path: 'b', loadChildren: () => import('./b/b.module').then(m => m.BModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
