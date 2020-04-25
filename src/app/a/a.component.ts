de314e9f8610a97a185083a4a20bf898ba2acf88
Eliran Eliassy <eliran.eliassy@gmail.com>
2020-04-25 17:50:31 +0300

    setup

--- src/app/a/a.component.ts
+++ src/app/a/a.component.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AComponent implements OnInit {
 
+  step1 = `
+  ng new v9-providedIn-example --routing --style=scss
+  ng add @angular/materail
+  ng generate @angular/material:navigation nav
+  ng g m a --route a --module app.module
+  ng g m b --route b --module app.module
+  `;
+
+  step2 = `
+    ng g lib store
+    ng g s store --project=store
+  `;
+  
   constructor(private store: StoreService) { }
 
   ngOnInit(): void {
