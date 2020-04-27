a3a502e7595b3c6e7989394c3cb2e0c67937283e
Eliran Eliassy <eliran.eliassy@gmail.com>
2020-04-28 00:16:31 +0300

    add step b

--- src/app/b/b.component.ts
+++ src/app/b/b.component.ts
@@ -8,6 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BComponent implements OnInit {
 
+  step1 = `
+  ng g m app2
+  ng g c app2 --module=app2
+  `;
+
+  step2 = `
+  platformBrowserDynamic().bootstrapModule(App2Module)
+  .catch(err => console.error(err));
+  `;
+
+  step3 = `
+  @NgModule({
+    declarations: [App2Component],
+    imports:  [
+      BrowserModule
+    ],
+    bootstrap: [App2Component]
+  })
+  export class App2Module { }
+  `;
+
+  step4 = `
+  ...
+  <body class="mat-typography">
+    <app-root></app-root>
+    <app-app2></app-app2>
+  </body>
+  ...
+  `;
+
   constructor(private store: StoreService) { }
 
   ngOnInit(): void {
